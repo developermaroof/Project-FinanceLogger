bc8a110ecf5c49ff03c0ce7e29fe1a47dba6b964
Maroof-Ali-Qureshi <123650922+Maroof-Ali-Qureshi@users.noreply.github.com>
2024-01-17 12:06:36 +0500

    Initial commit

---
+++ sandbox.ts
@@ -0,0 +1,11 @@
+const character = "maroof";
+
+console.log(character);
+
+const inputs = document.querySelectorAll("input");
+
+console.log(inputs);
+
+inputs.forEach((input) => {
+  console.log(input);
+});
