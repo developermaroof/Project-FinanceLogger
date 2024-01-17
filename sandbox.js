8e203794cf4ced24a98953f328580b367098b9ce
Maroof-Ali-Qureshi <123650922+Maroof-Ali-Qureshi@users.noreply.github.com>
2024-01-17 13:15:37 +0500

    Lesson4

--- sandbox.js
+++ sandbox.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 var character = "maroof";
 console.log(character);
 var inputs = document.querySelectorAll("input");
@@ -5,3 +6,35 @@ console.log(inputs);
 inputs.forEach(function (input) {
     console.log(input);
 });
+=======
+// arrays
+var names = ["Kabib", "Ronaldo", "Salam"];
+names.push("Messi");
+// names.push(23)
+// names[0]= 3
+var numbers = [1, 2, 3, 4, 5];
+numbers.push(0);
+// numbers.push('')
+// numbers[0]= '0'
+var mixed = ["kabib", 1, "Salam", 4, 5, "Ronaldo"];
+mixed.push("Messi");
+mixed.push(2);
+mixed[0] = 3;
+// objects
+var Professionalist = {
+    name: "Maroof",
+    age: 23,
+    profession: "developer",
+};
+Professionalist.name = "Maroof Ali";
+Professionalist.age = 24;
+// Professionalist.name = 10
+// Professionalist.age = '24'
+// Professionalist.skills = ['react','firebase' , 'typescript']
+Professionalist = {
+    name: "Maroof",
+    age: 23,
+    profession: "developer",
+    // skills: []
+};
+>>>>>>> Stashed changes
--- sandbox.ts
+++ sandbox.ts
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const character = "maroof";
 
 console.log(character);
@@ -9,3 +10,43 @@ console.log(inputs);
 inputs.forEach((input) => {
   console.log(input);
 });
+=======
+// arrays
+let names = ["Kabib", "Ronaldo", "Salam"];
+
+names.push("Messi");
+// names.push(23)
+// names[0]= 3
+
+let numbers = [1, 2, 3, 4, 5];
+
+numbers.push(0);
+// numbers.push('')
+// numbers[0]= '0'
+
+let mixed = ["kabib", 1, "Salam", 4, 5, "Ronaldo"];
+
+mixed.push("Messi");
+mixed.push(2);
+mixed[0] = 3;
+
+// objects
+let Professionalist = {
+  name: "Maroof",
+  age: 23,
+  profession: "developer",
+};
+
+Professionalist.name = "Maroof Ali";
+Professionalist.age = 24;
+// Professionalist.name = 10
+// Professionalist.age = '24'
+// Professionalist.skills = ['react','firebase' , 'typescript']
+
+Professionalist = {
+  name: "Maroof",
+  age: 23,
+  profession: "developer",
+  // skills: []
+};
+>>>>>>> Stashed changes
