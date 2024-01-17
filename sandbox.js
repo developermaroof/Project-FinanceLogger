dca3ee5ba76f5ace1b033a8d56786627cf1b8a63
Maroof-Ali-Qureshi <123650922+Maroof-Ali-Qureshi@users.noreply.github.com>
2024-01-17 18:54:09 +0500

    Added initial files

--- sandbox.js
+++
@@ -1,7 +0,0 @@
-var character = "maroof";
-console.log(character);
-var inputs = document.querySelectorAll("input");
-console.log(inputs);
-inputs.forEach(function (input) {
-    console.log(input);
-});
