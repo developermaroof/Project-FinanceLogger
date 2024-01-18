9d3fe8daa35a99bd8fe231f7408089f9f2c69bb2
Maroof-Ali-Qureshi <123650922+Maroof-Ali-Qureshi@users.noreply.github.com>
2024-01-18 21:14:25 +0500

    Updated

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+const form = document.querySelector(".new-item-form") as HTMLFormElement;
+// console.log(form.children);
+
+const type = document.querySelector("#type") as HTMLSelectElement;
+const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
+const details = document.querySelector("#details") as HTMLSelectElement;
+const amount = document.querySelector("#amount") as HTMLSelectElement;
+
+form.addEventListener("submit", (e: Event) => {
+  e.preventDefault();
+
+  console.log(type.value, tofrom.value, details.value, amount.value);
+});
